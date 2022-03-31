1743f24ad4d8933f19ccbb8eb448dbf6b6f5b83c
HenryZambrano <henryzambrano1503@gmail.com>
2022-03-31 18:59:10 -0500

    parcial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Historia from "./components/Historia"
+import Diseño from "./components/Diseño"
+
+
+function App() {
+  return (
+    <div className="layout">
+
+      
+      <Diseño/>
+
+    </div>
+  );
+}
+
+export default App;
